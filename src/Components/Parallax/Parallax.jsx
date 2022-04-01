6d896ae901e3569708ae9d378e91f4fe4a657ebb
myth114 <mythleshyadav114@gmail.com>
2022-04-02 00:41:24 +0545

    added parallax component and modified footer

---
+++ src/Components/Parallax/Parallax.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Parallax = () => {
+  return (
+    <section className='Section__Parallax'>
+      <div className='Parallax'></div>
+      <div className='Text'>
+        <h1>Achieve Your Goal With Collins</h1>
+        <span>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+          quas aut magnam, quos nam, animi amet voluptas fugiat facere
+          repudiandae pariatur qui quisquam esse earum commodi est, aliquam
+          architecto illum?
+        </span>
+        <button className='custom-btn btn-3'>Contact Us</button>
+      </div>
+    </section>
+  )
+}
+
+export default Parallax
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Footer from '../../Components/Footer/Footer'
 import HeroSection from '../../Components/HeroSection/HeroSection'
+import Parallax from '../../Components/Parallax/Parallax'
 import Services from '../../Components/Services/Services'
 import Testimonial from '../../Components/Testimonial/Testimonial'
 import TestPreparation from '../../Components/TestPreparation/TestPreparation'
-
 
 const LandingPage = () => {
   return (
@@ -12,9 +12,9 @@ const LandingPage = () => {
       <HeroSection />
       <TestPreparation />
       <Services />
-      <Testimonial/>
-      <Footer/>
-
+      <Parallax />
+      <Testimonial />
+      <Footer />
     </div>
   )
 }
